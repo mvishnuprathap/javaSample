4cf86896c35bf9a7c7461b4a922534394c86129a
VP <VP@VP>
2022-04-18 19:38:21 +0530

    TestCommitGitHub

---
+++ qwe.java/src/qwe/java/helloWorld.java
@@ -0,0 +1,11 @@
+package qwe.java;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello All of You");
+
+	}
+
+}
