56f133c7e4181a0515c78b094cdbd20c56df075c
VP <VP@VP>
2022-04-18 19:44:27 +0530

    CommitTest

--- qwe.java/src/qwe/java/helloWorld.java
+++ qwe.java/src/qwe/java/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello All of You");
+		System.out.println("Hello All of You Hello Friends");
 
 	}
 
