b78f779a34d1ac0248666228ab27f10e895e33b7
Vishnu <mvishnuprathap@gmail.com>
2022-05-03 20:53:02 +0530

    Added one extra printing line into the code as a part of studying git

--- qwe.java/src/qwe/java/helloWorld.java
+++ qwe.java/src/qwe/java/helloWorld.java
@@ -5,6 +5,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello All of You Hello Friends Hai all");
+		System.out.println("Hello Friends");
 
 	}
 
